de46ceb984f012d8f087474ad945fcaa96dafbb5
Atouk <romain.gaborieau@hotmail.fr>
2016-11-06 12:22:39 +0100

    Add a comment in the win start script

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
-set TWITTER_CONSUMER_KEY=mykey 
-set TWITTER_CONSUMER_SECRET=mysecret 
-set TWITTER_ACCESS_TOKEN_KEY=mytokenkey 
-set TWITTER_ACCESS_TOKEN_SECRET=mytokensecret 
+REM Enter here your twitter tokens. Be sur to not leave space at the end of the line
+
+set TWITTER_CONSUMER_KEY=mykey
+set TWITTER_CONSUMER_SECRET=mysecret
+set TWITTER_ACCESS_TOKEN_KEY=mytokenkey
+set TWITTER_ACCESS_TOKEN_SECRET=mytokensecret
 
 node index.js
